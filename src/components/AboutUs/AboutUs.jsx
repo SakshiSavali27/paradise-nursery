dace76059bb04f324a9210872c6f4e5e230b4847
Sakshi Savali <ssavali9559@gmail.com>
2026-09-15 19:58:27 +0530

    Create AboutUs.jsx

---
+++ src/components/AboutUs/AboutUs.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function AboutUs() {
+  return (
+    <div className="about-us">
+      <h2>About Us</h2>
+      <p>
+        Welcome to Paradise Nursery, where green meets serenity! We are passionate
+        about bringing nature closer to you with our curated collection of
+        beautiful, low-maintenance houseplants. Our mission is to help you create
+        your own green paradise at home, improving air quality and adding a calm,
+        refreshing vibe to any space.
+      </p>
+      <p>
+        Founded by a team of plant enthusiasts, Paradise Nursery sources healthy,
+        high-quality plants and delivers them straight to your door, along with
+        the care tips you need to help them thrive.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
